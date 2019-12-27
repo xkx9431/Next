d803c2eb611d723ef6b17937ccc2a7a4344433de
Xu Kaixuan <triumph_9431@qq.com>
2019-12-27 21:52:31 +0800

    add auth sample

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const Router = require('koa-router')
 const session = require('koa-session')
 const Redis = require('ioredis')
 const RedisSessionStore  = require('./server/session-store')
-
+const auth = require('./server/auth')
 const dev = process.env.NODE_ENV!='production'
 
 const app = next({ dev })
@@ -29,24 +29,8 @@ app.prepare().then( ()=>{
 
     server.use(session(SESSION_CONFIG,server))
 
-    server.use(async (ctx,next)=> {
-        if(!ctx.session.user){
-            ctx.session.user= {
-                name:'xkx',
-                age:18
-            } 
-        }else{
-            console.log(ctx.session.user)   
-        }
-
-        await next()
-
-    })
-
-
-
-
-    server.use(router.routes())
+    // 配置处理GitHub Oauth Login
+    auth(server)
 
     router.get('/a/:id', async (ctx)=>{
         const id  = ctx.params.id
@@ -56,23 +40,56 @@ app.prepare().then( ()=>{
         })
         ctx.respond  = false
     })
-
-    router.get('/set/user',  async (ctx)=>{
-        ctx.session.user  = {
-            name:'xkx',
-            age:25,
+    router.get('/api/user/info', async ctx => {
+        const user = ctx.session.userInfo
+        if (!user) {
+        ctx.status = 401
+        ctx.body = 'Need Login'
+        } else {
+        ctx.body = user
+        ctx.set('Content-Type', 'application/json')
         }
-        ctx.body = 'set session sucessful!'
     })
 
-    let index= 0
-    server.use( async (ctx,next)=>{
-        ctx.cookies.set('id',`id: ${index}`)
-        index+=1
-        await handle(ctx.req,ctx.res)
-        ctx.respond =false
-    })
+    // server.use(async (ctx,next)=> {
+    //     if(!ctx.session.user){
+    //         ctx.session.user= {
+    //             name:'xkx',
+    //             age:25
+    //         } 
+    //     }else{
+    //         console.log(ctx.session.user)   
+    //     }
 
+    //     await next()
+
+    // })
+    // router.get('/set/user',  async (ctx)=>{
+    //     ctx.session.user  = {
+    //         name:'xkx',
+    //         age:25,
+    //     }
+    //     ctx.body = 'set session sucessful!'
+    // })
+
+    // let index= 0
+    // server.use( async (ctx,next)=>{
+    //     ctx.cookies.set('id',`id: ${index}`)
+    //     index+=1
+    //     await handle(ctx.req,ctx.res)
+    //     ctx.respond =false
+    // })
+    server.use(router.routes())
+
+    server.use(async (ctx, next) => {
+        await handle(ctx.req, ctx.res)
+        ctx.respond = false
+    })
+    
+    server.use(async (ctx, next) => {
+        ctx.res.statusCode = 200
+        await next()
+    })
     server.listen(3000,()=>{
         console.log('koa server is listening localhost:3000')
     })
